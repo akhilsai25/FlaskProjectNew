40eccef32b326fcfe9b6cd8ac4641e2abc140938
akhilsai25 <38877327+akhilsai25@users.noreply.github.com>
2018-10-19 22:48:42 -0400

    Final update
    Changed the directory structure from windows to ubuntu for making it compatible with AWS instance.
    Added the necessary functional requirements.

--- assign.py
+++ assign.py
@@ -3,15 +3,19 @@ from flask import Flask, flash, request, redirect, url_for, render_template
 from werkzeug.utils import secure_filename
 from flask_uploads import UploadSet, configure_uploads, IMAGES, patch_request_class
 import datetime
+import PIL
 from PIL import Image
+import threading
 import PyPDF2
 import time
-UPLOAD_FOLDER = '/storage'
+from flask import send_from_directory
 
+UPLOAD_FOLDER = '/storage'
 ALLOWED_EXTENSIONS = set(['pdf', 'png', 'jpg', 'jpeg'])
 DIR_PATH=os.path.dirname(os.path.abspath(__file__))
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = DIR_PATH+UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 2 * 1024 * 1024
 filename=""
 file_type=" "
 file_size=" "
@@ -25,18 +29,16 @@ def pdf_count(filename,file_numpages):
 		text=extractData(file_path,i)
 		totalwords += getWordCount(text)
 		num_lines += text.count("\n")
-    time.sleep(1)
-    d.append(totalwords)
-    d.append(num_lines)
-    pdffileobj= open(file_path,'rb')
-    pdfreader=PyPDF2.PdfFileReader(pdffileobj)
-    d.append(pdfreader.getPage(0).mediaBox)
-    pdffileobj.close()
+        time.sleep(1)
+        d.append(totalwords)
+        d.append(num_lines)
+        pdffileobj= open(file_path,'rb')
+        pdfreader=PyPDF2.PdfFileReader(pdffileobj)
+        d.append(pdfreader.getPage(0).mediaBox)
+        pdffileobj.close()
+        return d
 
-    return d
-
-
- def extractData(file_path,i):
+def extractData(file_path,i):
         pdffileobj=open(file_path,'rb')
         pdfreader=PyPDF2.PdfFileReader(pdffileobj)
         pageobj=pdfreader.getPage(i)
@@ -44,13 +46,10 @@ def pdf_count(filename,file_numpages):
         pdffileobj.close()
         return data
 
-#files = UploadSet('files', extensions=('pdf','jpg'))
-#configure_uploads(app, files)
-#patch_request_class(app)  
+@app.route('/upload/<filename>')
+def send_image(filename):
+    return send_from_directory("storage", filename)
 
-#def timer_five_mins(filename):
-#    time.sleep(10)
- #   os.remove(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
 def allowed_file(filename):
     return '.' in filename and \
@@ -58,36 +57,44 @@ def allowed_file(filename):
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
+    file_path=''
     flag=0
+    d=[]
+    file_new=""
+    filename=""
     if request.method == 'POST':
+        file_type=filename[-3:].lower()
         # check if the post request has the file part
         if 'file' not in request.files:
             flash('No file part')
             return redirect(request.url)
-        file = request.files['file']
+        try:
+              file = request.files['file']
+        except:
+              return render_template("size_limit.html")
         mean=""
         image_size=""
-        d=""
+        d=[]
         # if user does not select file, browser also
         # submit an empty part without filename
         if file.filename == '':
             flash('No selected file')
             return redirect(request.url)
         if file and allowed_file(file.filename):
-
             result = request.form
             filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
-            
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'],filename)) 
             file_ctime=os.path.getctime(app.config['UPLOAD_FOLDER']+'/'+filename)
             file_ctime = datetime.datetime.fromtimestamp(file_ctime)
             file_mtime=os.path.getmtime(app.config['UPLOAD_FOLDER']+'/'+filename)
             file_mtime = datetime.datetime.fromtimestamp(file_mtime)
-
+            t=threading.Thread(target=wait_worker,args=(filename,))
+            t.start()
 
             
             if filename[-3:].lower() =='pdf':
-                file_type='pdf'
+                d=[]
+                file_type=filename[-3:].lower()
                 file_info=os.stat(app.config['UPLOAD_FOLDER']+'/'+filename)
                 file_sizebytes=file_info.st_size
                 check=open(app.config['UPLOAD_FOLDER']+'/'+filename,"rb")
@@ -113,6 +120,9 @@ def upload_file():
                 file_sizebytes=file_info.st_size
                 image_size=image.size
                 width, height = image_size
+                file_new=filename+'n'
+                file_path='../'+'storage/'+filename
+                # getThumb(filename,file_new)
                 total = 0
                 for i in range(0, width):
                      for j in range(0, height):
@@ -131,16 +141,16 @@ def upload_file():
         file_numpages=""
         totalWords=""
 
-    return render_template("hi.html",filename=filename,d=d,file_type=file_type,mean=mean,file_ctime=file_ctime,file_numpages=file_numpages,file_mtime=file_mtime,image_size=image_size,file_sizebytes=file_sizebytes)
+    return render_template("hi.html",file_path=file_path,filename=filename,d=d,file_type=file_type,mean=mean,file_ctime=file_ctime,file_numpages=file_numpages,file_mtime=file_mtime,image_size=image_size,file_sizebytes=file_sizebytes)
   
 
 
 	
-from flask import send_from_directory
+
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
-    return send_file(app.config['UPLOAD_FOLDER'],
-                               filename)
+    return send_from_directory("storage", filename)
+
 
 @app.route('/delete/<filename>')
 def delete_file(filename):
@@ -156,5 +166,13 @@ def getWordCount(data):
     data=data.split()
     return len(data)
 
+def wait_worker(filename):
+    time.sleep(300);
+    path=os.path.join(app.config['UPLOAD_FOLDER'],filename)
+    if(os.path.isfile(path)):
+       os.remove(path)
+    return
+
 if __name__ == '__main__':
     app.run(debug=TRUE)
+
